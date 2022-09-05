47a99e7f7eacba6d0ae8f72cff5c4816af86a97e
SvevKruger <111590788+SvevKruger@users.noreply.github.com>
2022-09-05 04:53:59 +0200

    Add files via upload

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+import random
+
+def guess_number(x):
+    guess_number = 0
+    random_number = random.randint(1, 20)
+    while guess_number != random_number:
+        guess_number = int(input("Insert a number between 1 and 20 "))
+        if guess_number > random_number:
+            print("Your number is too high")
+        elif guess_number < random_number:
+            print("Your number is too low")
+    if guess_number == random_number:
+        print(f"Congrats, that's right. The correct number is {guess_number}")
+
+guess_number(20)
+
